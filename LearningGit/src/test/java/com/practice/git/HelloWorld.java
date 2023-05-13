2d25c0d3b3d31619cf4aca22c6b74ee5f42bda71
franktraining <musabaytechcorp@gmail.com>
2023-05-13 11:30:46 -0400

    comment is removed, added new method

--- LearningGit/src/test/java/com/practice/git/HelloWorld.java
+++ LearningGit/src/test/java/com/practice/git/HelloWorld.java
@@ -2,9 +2,12 @@ package com.practice.git;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		// printing hello world string in the console
+	public static void main(String[] args) {	
 		System.out.println("Hello World");
 	}
 
+	
+	public void emptyMethod() {
+		
+	}
 }
