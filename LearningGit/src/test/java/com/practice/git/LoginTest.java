94fa02b8bea9c7d0a4c042dab7216680eb7179db
franktraining <musabaytechcorp@gmail.com>
2023-05-13 13:11:26 -0400

    added new main method

--- LearningGit/src/test/java/com/practice/git/LoginTest.java
+++ LearningGit/src/test/java/com/practice/git/LoginTest.java
@@ -2,4 +2,8 @@ package com.practice.git;
 
 public class LoginTest {
 
+
+	public static void main(String[] args) {
+		System.out.println("hello all, I am Jonny. I glad i joined this team !");
+	}
 }
